bac4599aea074ae3ee7629367c2257c028336800
Richard <rsaxvc@rsaxvc.net>
2016-02-04 20:31:11 -0600

    add more CRCs

--- collision_checker.py
+++ collision_checker.py
@@ -1,10 +1,21 @@
+from PyCRC.CRC16 import CRC16
+from PyCRC.CRC16DNP import CRC16DNP
+from PyCRC.CRC16SICK import CRC16SICK
+from PyCRC.CRC16Kermit import CRC16Kermit
+from PyCRC.CRC32 import CRC32
+from PyCRC.CRCCCITT import CRCCCITT
+
 import zlib
 import crc16
 
 crc_table = dict()
+crc_table["crc16"] = ( CRC16().calculate, dict() )
+crc_table["crc16dnp"] = ( CRC16DNP().calculate, dict() )
+crc_table["crc16sick"] = ( CRC16SICK().calculate, dict() )
+crc_table["crc16kermit"] = ( CRC16Kermit().calculate, dict() )
+crc_table["crc16xmodem"] = ( crc16.crc16xmodem, dict() )
 crc_table["adler32"] = ( zlib.adler32, dict() )
 crc_table["crc32"] = ( zlib.crc32, dict() )
-crc_table["crc16"] = ( crc16.crc16xmodem, dict() )
 
 import fileinput
 for line in fileinput.input():
